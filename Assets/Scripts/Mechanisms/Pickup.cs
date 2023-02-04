2382a523632e6ed94796e8a0f84d0e7953de83d8
Gabriela Bohorquez <gaby030300@hotmail.com>
2023-02-04 00:21:49 -0500

    Pick and drop - Sun Controller
    Pick and drop - Sun Controller

---
+++ Assets/Scripts/Mechanisms/Pickup.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    private Transform pickupPoint;
+    private Transform player;
+
+    [SerializeField] private float pickupDistance;
+    [SerializeField] private float minDistance;
+
+    public bool isReadyToThrow;
+    public bool isPicked;
+
+    private Rigidbody rb;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        player = GameObject.Find("Player").transform;
+        pickupPoint = GameObject.Find("Pickup Point").transform;
+
+    }
+    private void Update()
+    {
+        PickObject();
+    }
+
+    void PickObject()
+    { 
+        
+        pickupDistance = Vector3.Distance(player.position, transform.position);
+
+        if(pickupDistance <= minDistance)
+        {
+            if (Input.GetKeyDown(KeyCode.E) && isPicked == false && pickupPoint.childCount < 1)
+            {
+                GetComponent<Rigidbody>().useGravity = false;
+
+                rb.constraints = RigidbodyConstraints.FreezeAll;
+
+                GetComponent<BoxCollider>().enabled = false;
+                this.transform.position = pickupPoint.position;
+                this.transform.parent = GameObject.Find("Pickup Point").transform;
+
+                isPicked = true;
+            }
+
+        }
+
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            this.transform.parent = null;
+            GetComponent<Rigidbody>().useGravity = true;
+            GetComponent<BoxCollider>().enabled = true;
+
+            rb.constraints = RigidbodyConstraints.None;
+
+            isPicked = false;
+            isReadyToThrow = false;
+        }
+    }
+
+
+}
